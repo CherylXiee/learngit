18a07cfdacd34aee06cdb197a0c4b307e39a258f
Cheryl Xie <2478858032@qq.com>
2024-08-30 12:59:03 +0800

    wrote a gittest.cpp file

---
+++ gittest.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c,d,e,f,flag;
+    c=0;
+    flag=0;
+	scanf("%d %d",&a,&b);
+	while(c<=b){
+		scanf("%d",&d);
+		c++;
+		if(d==a&&c==1){
+			printf("Bingo!");
+			flag=1;
+			break;
+		}else if(d==a&&c<=3){
+			printf("Lucky You!");
+			flag=1;
+			break;
+		}else if(d==a&&c>3){
+			printf("Good Guess!");
+			flag=1;
+			break;
+		}else if(d>a){
+			printf("Too big\n");
+			flag=0;
+		}else{
+			printf("Too small\n");
+			flag=0;
+		}
+	}
+	if(flag==0){
+		printf("Game Over");
+	}
+
+        
+	printf("%d",d-e+f-c);
+	return 0;
+	}
